6af256b36ce9597e57aec80a62d21f52bd128a95
NOSFERATU-ll <nosferatu.ai01@gmail.com>
2026-08-15 17:37:37 +0200

    Port Blood Assembler upgrade card tooltips

--- src/main/java/com/nosferatu/divinemachinerylegacy/item/ItemBloodMachineUpgrade.java
+++ src/main/java/com/nosferatu/divinemachinerylegacy/item/ItemBloodMachineUpgrade.java
@@ -1,7 +1,13 @@
 package com.nosferatu.divinemachinerylegacy.item;
 
 import com.nosferatu.divinemachinerylegacy.DivineMachineryLegacy;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
+
+import java.util.List;
 
 /** Speed/parallel upgrade card used by the Blood Altar Assembler. */
 public class ItemBloodMachineUpgrade extends Item {
@@ -24,5 +30,14 @@ public class ItemBloodMachineUpgrade extends Item {
 
     public Type getType() {
         return type;
+    }
+
+    @Override
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean advanced) {
+        String key = type == Type.PARALLEL
+                ? "tooltip.divinemachinerylegacy.blood_altar_parallel_card.description"
+                : "tooltip.divinemachinerylegacy.blood_magic_speed_card.description";
+        lines.add(EnumChatFormatting.GRAY + StatCollector.translateToLocal(key));
     }
 }
