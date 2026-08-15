ab8b3f54fd721e130d7dc091f6fd09eaa03bb43e
NOSFERATU-ll <nosferatu.ai01@gmail.com>
2026-08-15 08:37:37 +0200

    Use permitted Extra Reforked catalyst textures

--- src/main/java/com/nosferatu/divinemachinerylegacy/item/ItemMachineCatalyst.java
+++ src/main/java/com/nosferatu/divinemachinerylegacy/item/ItemMachineCatalyst.java
@@ -4,15 +4,15 @@ import com.nosferatu.divinemachinerylegacy.DivineMachineryLegacy;
 import net.minecraft.item.Item;
 
 /**
- * Simple 1.7.10 representation of Extra Reforked machine catalysts.
- * Proper artwork will replace the temporary vanilla icon later.
+ * 1.7.10 representation of Extra Reforked machine catalysts.
+ * Artwork is used with explicit upstream permission; see docs/.
  */
 public class ItemMachineCatalyst extends Item {
 
-    public ItemMachineCatalyst(String key, String placeholderTexture) {
+    public ItemMachineCatalyst(String key, String textureKey) {
         setUnlocalizedName(DivineMachineryLegacy.MODID + "." + key);
         setCreativeTab(DivineMachineryLegacy.CREATIVE_TAB);
         setMaxStackSize(1);
-        setTextureName(placeholderTexture);
+        setTextureName(DivineMachineryLegacy.MODID + ":reforked/" + textureKey);
     }
 }
