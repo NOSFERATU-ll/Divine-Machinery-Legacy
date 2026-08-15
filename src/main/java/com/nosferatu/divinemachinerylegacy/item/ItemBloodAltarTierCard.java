cc7287091af5e92cc036293af389ad55f1767f90
NOSFERATU-ll <nosferatu.ai01@gmail.com>
2026-08-15 17:37:25 +0200

    Port Blood Altar tier card tooltips

--- src/main/java/com/nosferatu/divinemachinerylegacy/item/ItemBloodAltarTierCard.java
+++ src/main/java/com/nosferatu/divinemachinerylegacy/item/ItemBloodAltarTierCard.java
@@ -1,7 +1,13 @@
 package com.nosferatu.divinemachinerylegacy.item;
 
 import com.nosferatu.divinemachinerylegacy.DivineMachineryLegacy;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
+
+import java.util.List;
 
 /** Blood Altar tier card backported from bmaddon. */
 public class ItemBloodAltarTierCard extends Item {
@@ -18,5 +24,14 @@ public class ItemBloodAltarTierCard extends Item {
 
     public int getTier() {
         return tier;
+    }
+
+    @Override
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean advanced) {
+        lines.add(EnumChatFormatting.DARK_RED + StatCollector.translateToLocalFormatted(
+                "tooltip.divinemachinerylegacy.blood_altar_tier_card.tier", tier));
+        lines.add(EnumChatFormatting.GRAY + StatCollector.translateToLocal(
+                "tooltip.divinemachinerylegacy.blood_altar_tier_card.description"));
     }
 }
