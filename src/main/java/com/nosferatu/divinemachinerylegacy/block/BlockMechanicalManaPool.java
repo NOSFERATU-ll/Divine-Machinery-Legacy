d0fe80fe309a7001359772568943a2e72c1b353c
NOSFERATU-ll <nosferatu.ai01@gmail.com>
2026-08-15 11:07:10 +0200

    Use the real Mechanical Mana Pool renderer

--- src/main/java/com/nosferatu/divinemachinerylegacy/block/BlockMechanicalManaPool.java
+++ src/main/java/com/nosferatu/divinemachinerylegacy/block/BlockMechanicalManaPool.java
@@ -17,86 +17,49 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import vazkii.botania.common.block.ModFluffBlocks;
 
 import java.util.List;
 
 public class BlockMechanicalManaPool extends BlockContainer {
+    private static int renderId;
+    @SideOnly(Side.CLIENT) private IIcon[] tierIcons;
 
-    @SideOnly(Side.CLIENT)
-    private IIcon[] tierIcons;
-
-    public BlockMechanicalManaPool() {
+    public BlockMechanicalManaPool(){
         super(Material.rock);
-        setBlockName(DivineMachineryLegacy.MODID + ".mechanical_mana_pool");
-        setHardness(4.0F);
-        setResistance(10.0F);
-        setCreativeTab(DivineMachineryLegacy.CREATIVE_TAB);
+        setBlockName(DivineMachineryLegacy.MODID+".mechanical_mana_pool");
+        setHardness(4F); setResistance(10F); setLightOpacity(0); setCreativeTab(DivineMachineryLegacy.CREATIVE_TAB);
     }
 
-    @Override
-    public TileEntity createNewTileEntity(World world, int meta) {
-        return new TileMechanicalManaPool();
+    public static void setRenderId(int id){renderId=id;}
+    @Override public TileEntity createNewTileEntity(World world,int meta){return new TileMechanicalManaPool();}
+    @Override public int damageDropped(int meta){return meta&3;}
+    @Override protected ItemStack createStackedBlock(int meta){return new ItemStack(this,1,meta&3);}
+    @Override @SideOnly(Side.CLIENT) public void getSubBlocks(Item item,net.minecraft.creativetab.CreativeTabs tab,List list){for(int i=0;i<4;i++)list.add(new ItemStack(item,1,i));}
+    @Override public boolean isOpaqueCube(){return false;}
+    @Override public boolean renderAsNormalBlock(){return false;}
+    @Override public int getRenderType(){return renderId;}
+
+    @Override @SideOnly(Side.CLIENT) public IIcon getIcon(int side,int meta){return getFrameIcon(meta);}
+    @SideOnly(Side.CLIENT) public IIcon getFrameIcon(int meta){int i=MachineTier.fromMeta(meta).ordinal();return tierIcons!=null&&tierIcons[i]!=null?tierIcons[i]:blockIcon;}
+    @SideOnly(Side.CLIENT) public IIcon getPoolIcon(){return ModFluffBlocks.biomeStoneA.getIcon(1,0);}
+
+    @Override @SideOnly(Side.CLIENT) public void registerBlockIcons(IIconRegister r){
+        tierIcons=new IIcon[4];
+        tierIcons[0]=r.registerIcon(DivineMachineryLegacy.MODID+":reforked/malachite_dragonstone_block");
+        tierIcons[1]=r.registerIcon(DivineMachineryLegacy.MODID+":reforked/saffron_dragonstone_block");
+        tierIcons[2]=r.registerIcon(DivineMachineryLegacy.MODID+":reforked/shadow_dragonstone_block");
+        tierIcons[3]=r.registerIcon(DivineMachineryLegacy.MODID+":reforked/crimson_dragonstone_block");
+        blockIcon=tierIcons[0];
     }
 
-    @Override
-    public int damageDropped(int meta) {
-        return meta & 3;
+    @Override public boolean onBlockActivated(World world,int x,int y,int z,EntityPlayer p,int side,float hx,float hy,float hz){
+        if(!world.isRemote)p.openGui(DivineMachineryLegacy.INSTANCE,DivineMachineryLegacy.GUI_MANA_POOL,world,x,y,z); return true;
     }
 
-    @Override
-    protected ItemStack createStackedBlock(int meta) {
-        return new ItemStack(this, 1, meta & 3);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void getSubBlocks(Item item, net.minecraft.creativetab.CreativeTabs tab, List list) {
-        for (int meta = 0; meta < 4; meta++) list.add(new ItemStack(item, 1, meta));
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public IIcon getIcon(int side, int meta) {
-        int index = MachineTier.fromMeta(meta).ordinal();
-        if (tierIcons != null && index >= 0 && index < tierIcons.length && tierIcons[index] != null) return tierIcons[index];
-        return blockIcon;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister register) {
-        tierIcons = new IIcon[4];
-        tierIcons[MachineTier.MALACHITE.ordinal()] = register.registerIcon(DivineMachineryLegacy.MODID + ":reforked/malachite_dragonstone_block");
-        tierIcons[MachineTier.SAFFRON.ordinal()] = register.registerIcon(DivineMachineryLegacy.MODID + ":reforked/saffron_dragonstone_block");
-        tierIcons[MachineTier.SHADOW.ordinal()] = register.registerIcon(DivineMachineryLegacy.MODID + ":reforked/shadow_dragonstone_block");
-        tierIcons[MachineTier.CRIMSON.ordinal()] = register.registerIcon(DivineMachineryLegacy.MODID + ":reforked/crimson_dragonstone_block");
-        blockIcon = tierIcons[0];
-    }
-
-    @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player,
-                                    int side, float hitX, float hitY, float hitZ) {
-        if (!world.isRemote) {
-            player.openGui(DivineMachineryLegacy.INSTANCE, DivineMachineryLegacy.GUI_MANA_POOL, world, x, y, z);
-        }
-        return true;
-    }
-
-    @Override
-    public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
-        TileEntity te = world.getTileEntity(x, y, z);
-        if (te instanceof IInventory) {
-            IInventory inv = (IInventory) te;
-            for (int slot = 0; slot < inv.getSizeInventory(); slot++) {
-                ItemStack stack = inv.getStackInSlot(slot);
-                if (stack == null) continue;
-                EntityItem drop = new EntityItem(world, x + 0.5D, y + 0.5D, z + 0.5D, stack.copy());
-                drop.motionX = world.rand.nextGaussian() * 0.05D;
-                drop.motionY = world.rand.nextGaussian() * 0.05D + 0.2D;
-                drop.motionZ = world.rand.nextGaussian() * 0.05D;
-                world.spawnEntityInWorld(drop);
-            }
-        }
-        super.breakBlock(world, x, y, z, block, meta);
+    @Override public void breakBlock(World world,int x,int y,int z,Block block,int meta){
+        TileEntity te=world.getTileEntity(x,y,z);
+        if(te instanceof IInventory){IInventory inv=(IInventory)te;for(int s=0;s<inv.getSizeInventory();s++){ItemStack stack=inv.getStackInSlot(s);if(stack==null)continue;EntityItem drop=new EntityItem(world,x+.5,y+.5,z+.5,stack.copy());drop.motionX=world.rand.nextGaussian()*.05;drop.motionY=world.rand.nextGaussian()*.05+.2;drop.motionZ=world.rand.nextGaussian()*.05;world.spawnEntityInWorld(drop);}}
+        super.breakBlock(world,x,y,z,block,meta);
     }
 }
