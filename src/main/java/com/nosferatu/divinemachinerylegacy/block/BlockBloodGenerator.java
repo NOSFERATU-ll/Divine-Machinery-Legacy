f97e4a72ea7345cc76d92d9775bd3d25d07dc375
NOSFERATU-ll <nosferatu.ai01@gmail.com>
2026-08-15 17:34:35 +0200

    Use TESR for Blood Generator model

--- src/main/java/com/nosferatu/divinemachinerylegacy/block/BlockBloodGenerator.java
+++ src/main/java/com/nosferatu/divinemachinerylegacy/block/BlockBloodGenerator.java
@@ -26,6 +26,21 @@ public class BlockBloodGenerator extends BlockContainer {
     }
 
     @Override
+    public int getRenderType() {
+        return -1;
+    }
+
+    @Override
+    public boolean renderAsNormalBlock() {
+        return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube() {
+        return false;
+    }
+
+    @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player,
                                     int side, float hitX, float hitY, float hitZ) {
         if (!world.isRemote) {
