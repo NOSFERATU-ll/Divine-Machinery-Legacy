e69679b7f0d9ce3b0a2859b0702b33644f7accdc
NOSFERATU-ll <nosferatu.ai01@gmail.com>
2026-08-15 17:07:40 +0200

    Register Blood Pattern and original recipe

--- src/main/java/com/nosferatu/divinemachinerylegacy/bloodmagic/BloodMagicContent.java
+++ src/main/java/com/nosferatu/divinemachinerylegacy/bloodmagic/BloodMagicContent.java
@@ -1,10 +1,15 @@
 package com.nosferatu.divinemachinerylegacy.bloodmagic;
 
+import WayofTime.alchemicalWizardry.ModItems;
+import appeng.api.AEApi;
 import com.nosferatu.divinemachinerylegacy.block.BlockBloodGenerator;
 import com.nosferatu.divinemachinerylegacy.config.BloodMagicAddonConfig;
+import com.nosferatu.divinemachinerylegacy.item.ItemBloodAltarPattern;
 import com.nosferatu.divinemachinerylegacy.tile.TileBloodGenerator;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 import java.io.File;
 
@@ -17,6 +22,7 @@ public final class BloodMagicContent {
     public static final int GUI_BLOOD_GENERATOR = 12;
 
     public static Block bloodGenerator;
+    public static Item bloodAltarPattern;
 
     private static boolean registered;
 
@@ -32,5 +38,17 @@ public final class BloodMagicContent {
         GameRegistry.registerBlock(bloodGenerator, "blood_generator");
         GameRegistry.registerTileEntity(TileBloodGenerator.class,
                 "divinemachinerylegacy.blood_generator");
+
+        bloodAltarPattern = new ItemBloodAltarPattern();
+        GameRegistry.registerItem(bloodAltarPattern, "blood_altar_pattern");
+
+        ItemStack aeBlankPattern = AEApi.instance().definitions().materials().blankPattern().maybeStack(1).orNull();
+        if (aeBlankPattern != null && ModItems.weakBloodShard != null) {
+            // Exact bmaddon recipe: four Weak Blood Shards around one AE2 blank pattern.
+            GameRegistry.addRecipe(new ItemStack(bloodAltarPattern),
+                    " S ", "SPS", " S ",
+                    'S', new ItemStack(ModItems.weakBloodShard),
+                    'P', aeBlankPattern);
+        }
     }
 }
